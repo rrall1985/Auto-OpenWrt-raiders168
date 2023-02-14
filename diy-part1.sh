6b10101a340df2ce8eb72ddfd82154ce3f8f22cf
rrall1985 <31793014+rrall1985@users.noreply.github.com>
2023-02-15 01:59:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,8 +28,3 @@ echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>fe
 
 ####### darkmatter主题 #######
 #echo 'src-git darkmatter git://github.com/apollo-ng/luci-theme-darkmatter.git' >>feeds.conf.default
-
-########### 安装mosdnsv5###########
-git clone https://github.com/QiuSimons/openwrt-mos package/m#osdns
-# git clone https://github.com/QiuSimons/openwrt-mos/tree/master/v2ray-geodata package/v2ray-geodata
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
