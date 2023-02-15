24bec8fbf922ae261e759e6c5180f00e17bfcbcc
rrall1985 <31793014+rrall1985@users.noreply.github.com>
2023-02-16 04:10:22 +0800

    Update diy-6.1-part2.sh

--- diy-6.1-part2.sh
+++ diy-6.1-part2.sh
@@ -39,8 +39,8 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 # git clone https://github.com/pymumu/smartdns.git package/smartdns
 
 ########### 安装mosdnsv5###########
-rm -rf /feeds/packages/net/mosdns
+# rm -rf /feeds/packages/net/mosdns
 # rm -rf ./feeds/packages/net/v2ray-geodata
-git clone https://github.com/QiuSimons/openwrt-mos package/mosdns
+# git clone https://github.com/QiuSimons/openwrt-mos package/mosdns
 # git clone https://github.com/QiuSimons/openwrt-mos/tree/master/v2ray-geodata package/v2ray-geodata
 # git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
